a2f60c69083170d1411104aeb8c77b1bad5c7fea
Stephan Yu <Stephan.yu@gmail.com>
2014-11-17 18:52:24 +0000

    added default image url and set up images via paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141116172442) do
+ActiveRecord::Schema.define(version: 20141117015011) do
 
   create_table "listings", force: true do |t|
     t.string   "name"
     t.text     "description"
     t.decimal  "price"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
