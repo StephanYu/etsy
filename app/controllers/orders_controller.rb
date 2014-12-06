bc4035f7fcaefe803b1e1a4fb390a4a924b8bb4e
Stephan Yu <Stephan.yu@gmail.com>
2014-12-06 13:48:51 +0100

    created order and sales history pages and added drop down menus to header

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,22 +4,18 @@ class OrdersController < ApplicationController
 
   respond_to :html
 
-  def index
-    @orders = Order.all
-    respond_with(@orders)
+  def sales
+    @orders = Order.all.where(seller: current_user).order('created_at DESC')
   end
 
-  def show
-    respond_with(@order)
+  def purchases
+    @orders = Order.all.where(buyer: current_user).order('created_at DESC')
   end
 
   def new
     @order = Order.new
     @listing = Listing.find(params[:listing_id])
     respond_with(@order)
-  end
-
-  def edit
   end
 
   def create
@@ -35,16 +31,6 @@ class OrdersController < ApplicationController
     flash[:notice] = 'Order was successfully created.' if @order.save
 
     respond_with(@order, :location => root_url)
-  end
-
-  def update
-    @order.update(order_params)
-    respond_with(@order)
-  end
-
-  def destroy
-    @order.destroy
-    respond_with(@order)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   
   resources :listings do
-    resources :orders
+    resources :orders, only: [:new, :create]
   end
 
   root 'listings#index'
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
 
   get 'contact', to: 'pages#contact'
+
+  get 'sales' => 'orders#sales'
+  get 'purchases' => 'orders#purchases'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
