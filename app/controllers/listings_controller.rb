375d75e58cd8cd21010e74dfc6b880bb0707d744
Stephan Yu <Stephan.yu@gmail.com>
2015-06-09 23:14:41 +0200

    CSS changes to listing index and show page. added etsy favicon and logo in header. removed unnecessary images from asset pipeline

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -12,10 +12,11 @@ class ListingsController < ApplicationController
   def index
     # binding.pry
     if params[:category].blank?
-      @listings = Listing.all.order('created_at DESC').paginate(:page => params[:page], :per_page => 4)
+      @listings = Listing.all.order('created_at DESC').paginate(:page => params[:page], :per_page => 8)
     else
-      @category_id = Category.find_by(name: params[:category]).id
-      @listings = Listing.where(category_id: @category_id).order('created_at DESC').paginate(:page => params[:page], :per_page => 4)
+      @category = Category.find_by(name: params[:category])
+      @category_id = @category.id
+      @listings = Listing.where(category_id: @category_id).order('created_at DESC').paginate(:page => params[:page], :per_page => 8)
     end
   end
 
