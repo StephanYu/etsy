d5d58ca727a782df53d070e41f08d7c2aa4fafa8
Stephan Yu <Stephan.yu@gmail.com>
2015-06-09 11:06:17 +0200

    added new data to seeds.rb file to test out dropbox image uploading via seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,18 +12,35 @@ categories.each do |category|
   Category.create(name: category)
 end
 
+User.create(first_name: "Stephan", last_name: "Yu", email: "stephan.yu@gmail.com", password: "12345678", password_confirmation: "12345678")
+
+
+# u = User.new({:name => 'username', :email => 'user@name.fr'...})
+# u.avartar = File.open('/Users/myAccount/avatars/user.png')
+# u.save!
 # Listing.create(...., image =File.open('http://dropbox/'))
-# listings = [
-#   {
-#     name:
-#     description:
-#     price:
-#     image_file_name:
-#     image_content_type:
-#     image_file_size:
-#     user_id:
-#     category_id:
-#   },
+
+listings = [
+  {
+    name: "Test",
+    description: "Test description",
+    price: 100,
+    image: File.open("https://dl.dropboxusercontent.com/1/view/wwlnr96k58yxvk9/Apps/Etsyreborn/original/1_normal_colour-flash-monogrammed-cushion.jpg"),
+    user_id: 1,
+    category_id: 1
+  }
+]
+
+listings.each do |listing|
+  Listing.create(
+    name: listing.name,
+    description: listing.description,
+    price: listing.price,
+    image: listing.image,
+    user_id: listing.user_id,
+    category_id: listing.category_id
+  )
+end
 #   {
 #     name:
 #     description:
