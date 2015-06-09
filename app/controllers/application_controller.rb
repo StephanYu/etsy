bdc76100b91853ad59b8ac2ae3c0efbed228ad85
Stephan Yu <Stephan.yu@gmail.com>
2015-06-09 14:32:51 +0200

    added star images to asset pipeline instead of dropbox folder. changed jumbotron background image. added admin column to users table. added basic validations to reviews model. added review date and author to listings show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :first_name
     devise_parameter_sanitizer.for(:sign_up) << :last_name
-    devise_parameter_sanitizer.for(:sign_up) << :email
-    devise_parameter_sanitizer.for(:sign_up) << :password
     devise_parameter_sanitizer.for(:account_update) << :first_name
     devise_parameter_sanitizer.for(:account_update) << :last_name
   end
--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -22,7 +22,7 @@ class ListingsController < ApplicationController
   # GET /listings/1
   # GET /listings/1.json
   def show
-    @reviews = Review.where(listing_id: @listing.id)
+    @reviews = Review.where(listing_id: @listing.id).order("created_at DESC")
     if @reviews.blank?
       @avg_rating = 0
     else
