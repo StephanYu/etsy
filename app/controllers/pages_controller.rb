6f26441f7edfaf9220d81e3d13ad7753bac6318d
Stephan Yu <Stephan.yu@gmail.com>
2015-06-11 10:45:53 +0200

    removed @categories from home page. centered pagination on listings index page. fixed back link on edit user page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,7 @@
 class PagesController < ApplicationController
   
   def home
-    if params[:category].blank?
-      @listings = Listing.all.order('created_at DESC').paginate(:page => params[:page], :per_page => 8)
-    else
-      @category = Category.find_by(name: params[:category])
-      @category_id = @category.id
-      @listings = Listing.where(category_id: @category_id).order('created_at DESC').paginate(:page => params[:page], :per_page => 8)
-    end
+    @listings = Listing.all.order('created_at DESC').paginate(:page => params[:page], :per_page => 8)
   end
 
   def about
